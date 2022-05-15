462487bbe0139a9a4f90d28f4148232613c0da65
DecryptAG <akshat.goel.civ21@itbhu.ac.in>
2022-05-15 22:05:26 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,18 @@
+
+setInterval(()=>{
+    d= new Date();
+    htime = d.getHours();
+    mtime = d.getMinutes();
+    stime = d.getSeconds();
+    hrotation = 30*htime+mtime/2;
+    mrotation = 6*mtime;
+    srotation = 6*stime;
+
+    document.getElementById("hour").style.transform = "rotate("+hrotation+"deg)";
+    document.getElementById("minute").style.transform = "rotate("+mrotation+"deg)";
+    document.getElementById("seconds").style.transform = "rotate("+srotation+"deg)";
+    
+},1000)
+
+var tick = new Audio("/Users/akshatgoel/Desktop/Web Development/Analog Clock/clock-ticking-2.mp3");
+tick.play();
